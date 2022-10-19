e022096947101f3f9bb7fb2eb5b718a70fc1ab3e
Calum Haughton <calumpeterhaughton@gmail.com>
2022-10-19 17:02:38 +0100

    Bulma updates and clean up

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "./assets/main.css";
-import "./assets/stylesheet.scss";
+import '@/assets/main.scss';
 
 createApp(App).mount("#app");
