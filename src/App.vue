1e7fe34c6deff8e3e20068eba8240d43c79ed450
Calum Haughton <calumpeterhaughton@gmail.com>
2022-10-21 10:27:54 +0100

    Added basic grid for drumpads

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,60 @@
 <template>
-  <button class="button">Let's get started</button>
-  Here's a test
+  <div class="container is-fluid">
+    <h1 class="is-size-3">DM</h1>
+    <div class="columns is-mobile is-multiline">
+      <DrumPad v-for="drum in drums" :drum="drum"/>
+    </div>
+  </div>
 </template>
+
+<script>
+  import DrumPad from './components/DrumPad.vue'
+
+  export default {
+    components: {
+      DrumPad
+    },
+    data () {
+      return {
+        drums: [
+          {
+            keybind: "q",
+            sound: "Heater-1"
+          },
+          {
+            keybind: "w",
+            sound: "Heater-2"
+          },
+          {
+            keybind: "e",
+            sound: "Heater-3"
+          },
+          {
+            keybind: "a",
+            sound: "Heater-4"
+          },
+          {
+            keybind: "s",
+            sound: "Open-HH"
+          },
+          {
+            keybind: "d",
+            sound: "Closed-HH"
+          },
+          {
+            keybind: "z",
+            sound: "Clap"
+          },
+          {
+            keybind: "x",
+            sound: "Kick-n-hat"
+          },
+          {
+            keybind: "c",
+            sound: "Kick"
+          }
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/components/DrumPad.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="column is-4">
+        <div class="has-background-primary">
+            <p class="is-size-3 
+                        has-text-centered 
+                        has-text-primary-light">
+                            {{drum.keybind.toUpperCase()}}
+                        </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DrumPad',
+        props: [
+            'drum'
+        ]
+    }
+</script>
+
+<style>
+* {
+    border:1px dashed black;
+}
+</style>
+
