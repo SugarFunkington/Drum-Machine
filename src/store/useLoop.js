675d65552d5da92db1ee0f960de0946163711e31
Calum Haughton <calumpeterhaughton@gmail.com>
2022-10-25 22:17:26 +0100

    Added time to drumbeat object recording

--- src/store/useLoop.js
+++ src/store/useLoop.js
@@ -3,10 +3,11 @@ import { defineStore } from 'pinia'
 export const useLoopStore = defineStore('main', {
     state: () => ({
         loopRecording: false,
+        drumbeatStartTime: 0,
+        drumbeatEndTime: 0,
         loops: []
     }),
     getters: {
-
     },
     actions: {
         recordNewLoop() {
@@ -15,10 +16,24 @@ export const useLoopStore = defineStore('main', {
         },
         stopRecording() {
             this.loopRecording = false
+            // set both times to 0, adding time for the last drumbeat in the loop
         },
         addDrumbeat(drumbeat) {
-            this.loops[this.loops.length - 1].push(drumbeat)
-            //this.loops[-1].push(drumbeat)
-        }
+            // if start time = 0, add a start time
+            // if not, add an end time and calculate the timeElapsed
+            // add the timeElapsed to the previous drumbeat in the loop
+            if (!this.drumbeatStartTime) {
+                this.drumbeatStartTime = new Date().getTime()
+            } else {
+                this.drumbeatEndTime = new Date().getTime()
+                let timeElapsed = this.drumbeatEndTime - this.drumbeatStartTime
+                this.loops[this.loops.length - 1].at(-1).time = timeElapsed
+            }
+
+            this.loops[this.loops.length - 1].push({
+                drumbeat: drumbeat,
+                time: 0
+            })
+        },
     }
 })
