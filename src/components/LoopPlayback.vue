730785a22ef505481f365ee381d9db39cf441926
Calum Haughton <calumpeterhaughton@gmail.com>
2022-11-09 22:11:27 +0000

    Added animation to progress bar

--- src/components/LoopPlayback.vue
+++ src/components/LoopPlayback.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-2 is-flex is-flex-direction-row is-justify-content-space-between">
 
-        <button @click="this.loopPlayLoop(index)" v-if="!this.playingLoop">
+        <button @click="this.loopPlayLoop(index, loop)" v-if="!this.playingLoop">
             <font-awesome-icon icon="fa-solid fa-play"/>
         </button>
 
@@ -9,14 +9,19 @@
             <font-awesome-icon icon="fa-solid fa-pause"/>
         </button>
 
-        <div class="beats">
-            <span v-for="drum in loop" class="drumbeat-playback has-text-light has-text-centered">{{drum.drumbeat.toUpperCase()}}</span>
+        <div class="drumbeat-progress-indicator">
+            <div class="drumbeats is-flex is-flex-direction-row is-justify-content-center">
+                <span v-for="drum in loop" class="drumbeat-playback has-text-light has-text-centered is-unselectable">{{drum.drumbeat.toUpperCase()}}</span>
+            </div>
+            <div class="progress-bar-bg">
+                <div class="progress-bar" :class="{'in-progress': this.playingLoop}"></div>
+            </div>
         </div>
         
         <button @click="this.store.deleteLoop(index)">
             <font-awesome-icon icon="fa-solid fa-trash-can"/>
         </button>
-
+        
     </div>
 </template>
 
@@ -31,11 +36,13 @@ export default {
     data () {
         return {
             store: useLoopStore(),
-            playingLoop: false
+            playingLoop: false,
+            totalLoopLength: 0,
         }
     },
     methods: {
-        async loopPlayLoop(index) {
+        async loopPlayLoop(index, loop) {
+            this.setTotalLoopLength(loop)
             this.playingLoop = true
 
             while (this.playingLoop) {
@@ -44,6 +51,13 @@ export default {
         },
         pauseLoop() {
             this.playingLoop = false
+        },
+        setTotalLoopLength(loop) {
+            this.totalLoopLength = loop.reduce((pV,cV) => {
+                return pV += cV.pause
+            }, 0)
+
+            this.totalLoopLength += 'ms'
         }
     }
 }
@@ -51,14 +65,54 @@ export default {
 
 <style scoped lang="scss">
 
-.beats {
+@keyframes inProgress {
+    0% {
+        width:0%;
+    }
+    100% {
+        width:100%;
+    }
+}
+.progress-bar-bg {
+    width:100%;
+    height:1px;
+    background-color:$dark;
+    position:absolute;
+    top:19px;
+    left:0px;
+    z-index:1;
+}
+.progress-bar {
+    width:5%;
+    height:100%;
+    background-color:$light;
+}
+
+.in-progress {
+    animation-name: inProgress;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-duration:v-bind('totalLoopLength');
+}
+
+.drumbeats {
+    position:relative;
+    z-index:2;
+}
+
+.drumbeat-progress-indicator {
     display:inline-block;
+    height:38px;
+    width:100%;
+    position:relative;
 }
 .drumbeat-playback {
     display:inline-block;
-    border:1px solid $light;
+    border:1px solid $dark;
     border-radius:100%;
-    padding:2px 8px;
+    padding:2px 9px;
+    margin:4px;
+    background-color:$background;
 }
 
 button {
@@ -66,6 +120,7 @@ button {
     border:none;
     color:$light;
     width:24px;
+    cursor:pointer;
 }
 
 </style>
